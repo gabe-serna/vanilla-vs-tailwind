b24b705409f4b8c71a99bea2f6f04425b4d8d821
Gabe Serna <gabriel.serna118@gmail.com>
2024-09-25 17:14:53 -0400

    Create Output Block Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import { Code, Heading1, Paragraph } from "./components/Typography";
+import {
+  Code,
+  CodeBlock,
+  Heading1,
+  Output,
+  Paragraph
+} from "./components/Typography";
 
 function App() {
   return (
     <>
-      <Heading1>Heading</Heading1>
-      <Paragraph>Paragraph</Paragraph>
-      <Code caption={"Vanilla CSS"} className="w-1/3">
-        Code {"\n"}apple{"\n"}apple
-      </Code>
+      <Heading1>How to Write CSS 10x Faster in 5 minutes</Heading1>
+      <Paragraph className="mt-6">
+        If you want to write CSS Faster you need:{" "}
+        <strong className="font-medium">TailwindCSS</strong>.
+      </Paragraph>
+      <Paragraph>Look at the example below to see</Paragraph>
+      <CodeBlock>
+        <Code caption={"Vanilla CSS"} className="">
+          {"/* CSS */\nbody {\n  background-color: #f0f0f0;\n}"}
+        </Code>
+        <Output className="w-1/2 h-72">h1</Output>
+      </CodeBlock>
     </>
   );
 }
--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -43,17 +43,43 @@ interface CodeProps extends Props {
   caption?: string;
 }
 
-export const Code = ({ caption = "", children, className = "w-1/2" }: CodeProps) => {
+export const Code = ({ caption = "", children, className = "" }: CodeProps) => {
   return (
-    <figure className={className}>
-      <pre className="flex items-start 2xl:text-xl xl:text-lg lg:text-base text-sm font-mono text-white/70 bg-[#302d2d] rounded-md p-4 mt-4  h-min">
+    <figure className={"w-1/2 lg:w-1/3 min-w-min " + className}>
+      <pre className="flex items-start 2xl:text-xl xl:text-lg lg:text-base text-sm font-mono text-white/60 bg-[#302d2d] rounded-md p-4 mt-4 h-min min-w-min">
         {children}
       </pre>
       {caption && (
-        <figcaption className="text-right w-full 2xl:text-lg xl:text-base lg:text-sm text-xs font-light italic text-white/50">
+        <figcaption className="w-full text-xs italic font-light text-right 2xl:text-lg xl:text-base lg:text-sm text-white/50">
           {caption}
         </figcaption>
       )}
     </figure>
+  );
+};
+
+export const Output = ({ children, className = "w-1/2" }: Props) => {
+  return (
+    <div
+      className={
+        "flex items-start 2xl:text-xl xl:text-lg lg:text-base text-sm min-w-min font-mono text-white/60 bg-[#302d2d] rounded-md p-4 mt-4 " +
+        className
+      }
+    >
+      {children}
+    </div>
+  );
+};
+
+export const CodeBlock = ({ children, className }: Props) => {
+  return (
+    <div
+      className={
+        "flex lg:flex-row flex-col-reverse gap-6 align-center lg:justify-around items-center " +
+        className
+      }
+    >
+      {children}
+    </div>
   );
 };
