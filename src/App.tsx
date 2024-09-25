d1bfe78e6c82091575ef2392406d953daab7ac6b
Gabe Serna <gabriel.serna118@gmail.com>
2024-09-25 16:48:02 -0400

    Create Basic Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Code, Heading1, Paragraph } from "./components/Typography";
+
 function App() {
   return (
     <>
-      <h1>hello</h1>
+      <Heading1>Heading</Heading1>
+      <Paragraph>Paragraph</Paragraph>
+      <Code caption={"Vanilla CSS"} className="w-1/3">
+        Code {"\n"}apple{"\n"}apple
+      </Code>
     </>
   );
 }
--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -1,9 +1,59 @@
 interface Props {
   children: React.ReactNode;
+  className?: string;
 }
 
-const Heading1 = ({ children }: Props) => {
-  return <h1>{children}</h1>;
+export const Heading1 = ({ children, className }: Props) => {
+  return (
+    <h1
+      className={
+        "2xl:text-4xl xl:text-3xl lg:text-2xl text-xl font-bold " + className
+      }
+    >
+      {children}
+    </h1>
+  );
 };
 
-export default Heading1;
+export const Heading2 = ({ children, className }: Props) => {
+  return (
+    <h2
+      className={
+        "2xl:text-3xl xl:text-2xl lg:text-xl text-lg font-medium mt-2 " + className
+      }
+    >
+      {children}
+    </h2>
+  );
+};
+
+export const Paragraph = ({ children, className }: Props) => {
+  return (
+    <p
+      className={
+        "2xl:text-xl xl:text-lg lg:text-base text-sm font-thin mt-2 " + className
+      }
+    >
+      {children}
+    </p>
+  );
+};
+
+interface CodeProps extends Props {
+  caption?: string;
+}
+
+export const Code = ({ caption = "", children, className = "w-1/2" }: CodeProps) => {
+  return (
+    <figure className={className}>
+      <pre className="flex items-start 2xl:text-xl xl:text-lg lg:text-base text-sm font-mono text-white/70 bg-[#302d2d] rounded-md p-4 mt-4  h-min">
+        {children}
+      </pre>
+      {caption && (
+        <figcaption className="text-right w-full 2xl:text-lg xl:text-base lg:text-sm text-xs font-light italic text-white/50">
+          {caption}
+        </figcaption>
+      )}
+    </figure>
+  );
+};
