bd3ad7c9e4512429073fe7e075babe0ec31f5000
Gabe Serna <gabriel.serna118@gmail.com>
2024-09-25 20:16:07 -0400

    Add Section 1

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,137 @@ import {
   Code,
   CodeBlock,
   Heading1,
+  Heading2,
+  ListItem,
   Output,
-  Paragraph
+  Paragraph,
+  TextColumn
 } from "./components/Typography";
 
 function App() {
   return (
     <>
-      <Heading1>How to Write CSS 10x Faster in 5 minutes</Heading1>
-      <Paragraph className="mt-6">
-        If you want to write CSS Faster you need:{" "}
-        <strong className="font-medium">TailwindCSS</strong>.
-      </Paragraph>
-      <Paragraph>Look at the example below to see</Paragraph>
-      <CodeBlock>
-        <Code caption={"Vanilla CSS"} className="">
-          {"/* CSS */\nbody {\n  background-color: #f0f0f0;\n}"}
-        </Code>
-        <Output className="w-1/2 h-72">h1</Output>
-      </CodeBlock>
+      <section id="introduction">
+        <Heading1>How to Write CSS 10x Faster in 5 Minutes</Heading1>
+        <TextColumn>
+          <Paragraph>
+            Let's face it. Writing Vanilla CSS sucks. It's slow, tedious, and becomes
+            nightmarish to maintain as the project grows.
+          </Paragraph>
+          <Paragraph>
+            After this article, you'll never have to deal with that again, because
+            I'm going to show you a CSS solution that allows you to write styles
+            almost as fast as you think them.
+          </Paragraph>
+          <Paragraph className="mt-16">
+            If you want to write CSS blazingly fast you need:{" "}
+            <strong className="font-medium">TailwindCSS</strong>.
+          </Paragraph>
+          <Paragraph>
+            With Tailwind, you style elements by applying pre-existing utlity classes
+            directly in your HTML. If you've never seen styling like this before it
+            might look like heresy at first, but I'm going to share 5 reasons why
+            TailwindCSS will make you a much faster developer.
+          </Paragraph>
+        </TextColumn>
+      </section>
+      <section id="section-1">
+        <TextColumn>
+          <Heading2>There's Less to Write</Heading2>
+          <Paragraph>
+            Look at the example below to see the difference between how much code is
+            needed in Vanilla CSS and TailwindCSS to create the same effect.
+          </Paragraph>
+        </TextColumn>
+        <CodeBlock>
+          <Code caption={"Vanilla CSS"}>
+            {
+              ".circle {\n  width: 2.5rem;\n  height: 2.5rem;\n  border-radius: 1000px;\n  margin: 0 auto;\n  background-color: #2563eb;\n  transition-property: width height background-color;\n  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);\n  transition-duration: 150ms;\n\n  &:hover {\n    width: 3rem;\n    height: 3rem;\n    background-color: #dc2626;\n  }\n}"
+            }
+          </Code>
+          <Output>
+            <div className="circle" />
+          </Output>
+        </CodeBlock>
+        <CodeBlock>
+          <Code caption={"TailwindCSS"}>
+            {
+              '<div className="size-10 rounded-full mx-auto bg-blue-600 transition-[width_height_background-color] hover:size-12 hover:bg-red-600" />'
+            }
+          </Code>
+          <Output>
+            <div className="size-10 rounded-full mx-auto bg-blue-600 transition-[width_height_background-color] hover:size-12 hover:bg-red-600" />
+          </Output>
+        </CodeBlock>
+        <TextColumn className="mt-20">
+          <Paragraph>
+            There is one huge difference between the two examples above that's
+            immediately apparent: <em>the size difference</em>.
+          </Paragraph>
+          <Paragraph variant="quote">
+            TailwindCSS is simply faster to write than vanilla because there's{" "}
+            <u>less to write</u>.
+          </Paragraph>
+          <Paragraph>
+            With Tailwind, you can write a handleful of classes and achieve the same
+            functionality that would take you dozens of lines of CSS. In fact, let's
+            break down each class from the example above:
+          </Paragraph>
+          <ul className="ml-10 list-disc list-outside">
+            <ListItem>
+              <b>size-10</b>: Sets both the width and height to 2.5rem.
+            </ListItem>
+            <ListItem>
+              <b>rounded-full</b>: Sets border-radius to 1000px (a circle).
+            </ListItem>
+            <ListItem>
+              <b>mx-auto</b>: Sets the horizontal margin to auto. (
+              <em>Same as setting margin left and right to auto</em>)
+            </ListItem>
+            <ListItem>
+              <b>bg-blue-600</b>: Sets background-color to #2563eb.
+            </ListItem>
+            <ListItem>
+              <b>transition-[width_height_background-color]</b>: Sets
+              transition-property to width, height, and background-color, in addition
+              to setting the transition-timing-function and transition-duration.
+            </ListItem>
+            <ListItem>
+              <b>hover:size-12</b>: Sets both the width and height to 3rem when
+              hovered.
+            </ListItem>
+            <ListItem>
+              <b>hover:bg-red-600</b>: Sets background-color to #dc2626 when hovered.
+            </ListItem>
+          </ul>
+          <Paragraph className="mt-[4rem]">
+            Each Tailwind utility class is not only shorter than the property it
+            corresponds to, but some classes refer to multiple properties at once.
+            I'm sure you can already see how much faster this is than vanilla, but
+            we're just getting started.
+          </Paragraph>
+        </TextColumn>
+      </section>
+      <section id="section-2">
+        <TextColumn className="mt-20">
+          <Heading2>Responsive Design</Heading2>
+          <Paragraph>
+            If you've ever tried making a website responsive using Vanilla CSS, you
+            know that it quickly becomes utter chaos. Look at this simple example
+            below to show you what I mean.
+          </Paragraph>
+        </TextColumn>
+        <CodeBlock>
+          <Code caption={"TailwindCSS"}>
+            {
+              '<div className="size-10 rounded-full mx-auto bg-blue-600 transition-[width_height_background-color] hover:size-12 hover:bg-red-600" />'
+            }
+          </Code>
+          <Output>
+            <div className="w-full h-40 bg-blue-600 rounded-md" />
+          </Output>
+        </CodeBlock>
+      </section>
     </>
   );
 }
--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -7,7 +7,8 @@ export const Heading1 = ({ children, className }: Props) => {
   return (
     <h1
       className={
-        "2xl:text-4xl xl:text-3xl lg:text-2xl text-xl font-bold " + className
+        "2xl:text-4xl xl:text-3xl lg:text-2xl text-xl font-bold text-center " +
+        className
       }
     >
       {children}
@@ -19,7 +20,7 @@ export const Heading2 = ({ children, className }: Props) => {
   return (
     <h2
       className={
-        "2xl:text-3xl xl:text-2xl lg:text-xl text-lg font-medium mt-2 " + className
+        "2xl:text-2xl xl:text-xl lg:text-lg text-base font-bold mt-16 " + className
       }
     >
       {children}
@@ -27,15 +28,59 @@ export const Heading2 = ({ children, className }: Props) => {
   );
 };
 
-export const Paragraph = ({ children, className }: Props) => {
+interface ParagraphProps extends Props {
+  variant?: "default" | "quote";
+}
+
+export const Paragraph = ({
+  children,
+  className,
+  variant = "default"
+}: ParagraphProps) => {
+  if (variant === "default")
+    return (
+      <p
+        className={
+          "2xl:text-xl xl:text-lg lg:text-base text-sm font-thin mt-6 " + className
+        }
+      >
+        {children}
+      </p>
+    );
   return (
     <p
       className={
-        "2xl:text-xl xl:text-lg lg:text-base text-sm font-thin mt-2 " + className
+        "2xl:text-xl xl:text-lg lg:text-base text-sm font-light mt-6 text-center rounded-md italic bg-[var(--accent)] py-2 mx-auto  " +
+        className
       }
     >
       {children}
     </p>
+  );
+};
+
+export const TextColumn = ({ children, className }: Props) => {
+  return (
+    <div
+      style={{ maxWidth: "clamp(45ch, 50%, 75ch)" }}
+      className={
+        "mx-auto text-sm font-thin 2xl:text-xl xl:text-lg lg:text-base " + className
+      }
+    >
+      {children}
+    </div>
+  );
+};
+
+export const ListItem = ({ children, className }: Props) => {
+  return (
+    <li
+      className={
+        "2xl:text-xl xl:text-lg lg:text-base text-sm font-thin mt-6 " + className
+      }
+    >
+      {children}
+    </li>
   );
 };
 
@@ -45,8 +90,8 @@ interface CodeProps extends Props {
 
 export const Code = ({ caption = "", children, className = "" }: CodeProps) => {
   return (
-    <figure className={"w-1/2 lg:w-1/3 min-w-min " + className}>
-      <pre className="flex items-start 2xl:text-xl xl:text-lg lg:text-base text-sm font-mono text-white/60 bg-[#302d2d] rounded-md p-4 mt-4 h-min min-w-min">
+    <figure className={"w-full lg:w-1/3 min-w-min " + className}>
+      <pre className="flex items-start 2xl:text-xl xl:text-lg lg:text-base text-sm font-mono text-white/60 bg-[var(--card)] rounded-md p-4 mt-4 h-min min-w-min text-wrap">
         {children}
       </pre>
       {caption && (
@@ -58,11 +103,11 @@ export const Code = ({ caption = "", children, className = "" }: CodeProps) => {
   );
 };
 
-export const Output = ({ children, className = "w-1/2" }: Props) => {
+export const Output = ({ children, className = "w-full lg:w-1/3" }: Props) => {
   return (
     <div
       className={
-        "flex items-start 2xl:text-xl xl:text-lg lg:text-base text-sm min-w-min font-mono text-white/60 bg-[#302d2d] rounded-md p-4 mt-4 " +
+        "flex items-start 2xl:text-xl xl:text-lg lg:text-base text-sm min-w-min font-mono text-white/60 bg-[var(--card)] rounded-md p-4 lg:mt-0 mt-4 " +
         className
       }
     >
@@ -75,7 +120,7 @@ export const CodeBlock = ({ children, className }: Props) => {
   return (
     <div
       className={
-        "flex lg:flex-row flex-col-reverse gap-6 align-center lg:justify-around items-center " +
+        "flex lg:flex-row flex-col-reverse gap-6 align-center lg:justify-around items-center mt-16 " +
         className
       }
     >
